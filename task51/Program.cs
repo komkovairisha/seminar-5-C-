48c73a5f993ae0b056d025628be246ca19b1b79a
Irina Komkova <komkovairisha@gmail.com>
2023-03-05 23:24:29 +0300

    diagonal

---
+++ task51/Program.cs
@@ -0,0 +1,49 @@
+﻿// // Задача 51: Задайте двумерный массив. Найдите сумму элементов, 
+//находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12;
+int count = 0;
+int[,] FillMatrixWithRandom(int row, int column)
+{
+    int[,] matrix = new int[row, column];
+    ;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+            if (i == j)
+            {
+               count = count + matrix[i, j];
+            } 
+            
+
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + " ");
+        }
+    
+        Console.WriteLine();
+    }
+}
+
+System.Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+System.Console.Write(count);
+
